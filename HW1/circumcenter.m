a15d49433d276931217c5ad0f1f45507e1a38076
aprilnovak <novak@berkeley.edu>
2017-01-27 16:23:57 -0800

    adding refinements.

--- HW1/circumcenter.m
+++ HW1/circumcenter.m
@@ -1,28 +1,4 @@
-function [pt, max_area] = circumcenter(T, pv)
-% finds the circumcenter of the largest triangle
-% compute triangle areas using Heron's formula
-for i = 1:length(T(:,1))
-    A = [pv(T(i, 1), 1), pv(T(i, 1), 2)];
-    B = [pv(T(i, 2), 1), pv(T(i, 2), 2)];
-    C = [pv(T(i, 3), 1), pv(T(i, 3), 2)];
-    area(i) = (A(1) * (B(2) - C(2)) + B(1) * (C(2) - A(2)) + C(1) * (A(2) - B(2))) / 2;
-end
-
-% find the largest triangle, and store the number in refine
-max_area = area(1);
-refine = 1;
-for i = 1:length(area)
-    if area(i) > max_area
-        max_area = area(i);
-        refine = i;
-    end
-end
-
-sprintf('Refining triangle %i', refine)
-
-A = [pv(T(refine, 1), 1), pv(T(refine, 1), 2)];
-B = [pv(T(refine, 2), 1), pv(T(refine, 2), 2)];
-C = [pv(T(refine, 3), 1), pv(T(refine, 3), 2)];
+function [pt] = circumcenter(A, B, C, pv)
 
 AB = [(A(1) + B(1)) / 2, (A(2) + B(2)) / 2];
 m_AB = (B(2) - A(2)) / (B(1) - A(1));
--- HW1/pmesh.m
+++ HW1/pmesh.m
@@ -2,11 +2,10 @@
 clear all
 
 % number of uniform refinements
-nref = 0;
+nref = 1;
 
 % maximum element size
 hmax = 0.2;
-hmax = hmax/(2^nref);
 
 % original polygon boundary points
 pv = [0,0; 1,0; .5,.5; 1,1; 0,1; 0,0];
@@ -21,24 +20,46 @@ plot(pv(:,1), pv(:,2), 'o')
 
 max = hmax^2 / 2;
 max_area = max + 1; % dummy initial value
-for r = 1:30
+while max_area > max
     % find which points are outside the domain, then delete them
     [pv] = delete_outside(pv, pv_orig);
     
     % triangulate the domain
     T = delaunayn(pv);
 
-    % plot the domain
-    tplot(pv, T)
-
     % find which triangles are outside the domain, then delete them from T
     [T] = delete_outside_triangles(T, pv, pv_orig);
     tplot(pv, T)
     
-    % find the circumcenter of the largest triangle
-    [pt, max_area] = circumcenter(T, pv);
+    % compute triangle areas using Heron's formula
+    for i = 1:length(T(:,1))
+        A = [pv(T(i, 1), 1), pv(T(i, 1), 2)];
+        B = [pv(T(i, 2), 1), pv(T(i, 2), 2)];
+        C = [pv(T(i, 3), 1), pv(T(i, 3), 2)];
+        area(i) = (A(1) * (B(2) - C(2)) + B(1) * (C(2) - A(2)) + C(1) * (A(2) - B(2))) / 2;
+        if i == 1
+            max_area = area(1);
+        end
+        
+        if area(i) > max_area
+            max_area = area(i);
+            refine = i;
+        end 
+    end
+
+    A = [pv(T(refine, 1), 1), pv(T(refine, 1), 2)];
+    B = [pv(T(refine, 2), 1), pv(T(refine, 2), 2)];
+    C = [pv(T(refine, 3), 1), pv(T(refine, 3), 2)];
+    
+    [pt] = circumcenter(A, B, C, pv);
 
     % add the circumcenter to the list of points
     pv = [pv; pt];
+end
+
+% perform any uniform refinements
+for uf = 1:nref
+    sprintf('Performing uniform refinement %i', uf)
+    
 end
 
