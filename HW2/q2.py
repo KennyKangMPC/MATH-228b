629a0630f0b87f8bbfa20e196665a7073662f5b9
aprilnovak <novak@berkeley.edu>
2017-02-09 12:41:57 -0800

    made plots of shape functions.

--- HW2/q2.py
+++ HW2/q2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # number of elements
 num_elem = 2;
@@ -16,8 +17,8 @@ for j in range(num_elem):
 				B = np.empty([4, 1])
 				B[i] = 1
 				x = np.linalg.solve(A, B)
-				print("For element {0}, H{1}:".format(j+1, i+1))
-				print(x)
+				#print("For element {0}, H{1}:".format(j+1, i+1))
+				#print(x)
 
 				# verify correct calculation - zeroth derivatives
 				left = x[0,0]*(a**3) + x[1,0]*(a**2) + x[2,0]*a + x[3,0]
@@ -27,4 +28,30 @@ for j in range(num_elem):
 				left1 = 3.0 * x[0,0]*(a**2) + 2.0 * x[1,0]*a + x[2,0]
 				right1 = 3.0 * x[0,0]*(b**2) + 2.0 * x[1,0]*b + x[2,0]
 
-				print("{0} {1} {2} {3}".format(left, right, left1, right1))
+				#print("{0} {1} {2} {3}".format(left, right, left1, right1))
+				
+# plot the shape functions
+xx = np.linspace(0.0, 0.5, 100)
+xxx = np.linspace(0.5, 1.0, 100)
+
+H1_1 = 16*(xx**3) - 12*(xx**2) + 1
+H2_1 = -16*(xx**3) + 12*(xx**2)
+H3_1 = 4*(xx**3) - 4*(xx**2) + xx
+H4_1 = 4*(xx**3) - 2*(xx**2)
+
+H1_2 = 16*(xxx**3) - 36*(xxx**2) + 24*xxx - 4
+H2_2 = -16*(xxx**3) + 36*(xxx**2) - 24*xxx + 5
+H3_2 = 4*(xxx**3) - 10*(xxx**2) + 8*xxx - 2
+H4_2 = 4*(xxx**3) - 8*(xxx**2) + 5*xxx - 1
+
+plt.plot(xx, H1_1, linestyle='--', label='H1, e=1')
+plt.plot(xx, H2_1, linestyle='--', label='H2, e=1')
+plt.plot(xx, H3_1, linestyle='--',label='H3, e=1')
+plt.plot(xx, H4_1, linestyle='--',label='H4, e=1')
+plt.plot(xxx, H1_2, label='H1, e=2')
+plt.plot(xxx, H2_2, label='H2, e=2')
+plt.plot(xxx, H3_2, label='H3, e=2')
+plt.plot(xxx, H4_2, label='H4, e=2')
+plt.legend()
+
+plt.savefig('q2_Hermite_functions.png')
