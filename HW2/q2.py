eef645ec3894fa7cf3f34cc39409dbf5623282e5
aprilnovak <novak@berkeley.edu>
2017-02-09 13:42:06 -0800

    added element stiffness matrices.

--- HW2/q2.py
+++ HW2/q2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sympy as sp
 
 # number of elements
 num_elem = 2;
@@ -44,14 +45,35 @@ H2_2 = -16*(xxx**3) + 36*(xxx**2) - 24*xxx + 5
 H3_2 = 4*(xxx**3) - 10*(xxx**2) + 8*xxx - 2
 H4_2 = 4*(xxx**3) - 8*(xxx**2) + 5*xxx - 1
 
-plt.plot(xx, H1_1, linestyle='--', label='H1, e=1')
-plt.plot(xx, H2_1, linestyle='--', label='H2, e=1')
-plt.plot(xx, H3_1, linestyle='--',label='H3, e=1')
-plt.plot(xx, H4_1, linestyle='--',label='H4, e=1')
-plt.plot(xxx, H1_2, label='H1, e=2')
-plt.plot(xxx, H2_2, label='H2, e=2')
-plt.plot(xxx, H3_2, label='H3, e=2')
-plt.plot(xxx, H4_2, label='H4, e=2')
-plt.legend()
+#plt.plot(xx, H1_1, linestyle='--', label='H1, e=1')
+#plt.plot(xx, H2_1, linestyle='--', label='H2, e=1')
+#plt.plot(xx, H3_1, linestyle='--',label='H3, e=1')
+#plt.plot(xx, H4_1, linestyle='--',label='H4, e=1')
+#plt.plot(xxx, H1_2, label='H1, e=2')
+#plt.plot(xxx, H2_2, label='H2, e=2')
+#plt.plot(xxx, H3_2, label='H3, e=2')
+#plt.plot(xxx, H4_2, label='H4, e=2')
+#plt.legend()
 
-plt.savefig('q2_Hermite_functions.png')
+#plt.savefig('q2_Hermite_functions.png')
+
+# compute elemntal stiffness matrix K
+x = sp.Symbol('x')
+
+H1_1 = 16*(x**3) - 12*(x**2) + 1
+H2_1 = -16*(x**3) + 12*(x**2)
+H3_1 = 4*(x**3) - 4*(x**2) + x
+H4_1 = 4*(x**3) - 2*(x**2)
+
+H1_2 = 16*(x**3) - 36*(x**2) + 24*x - 4
+H2_2 = -16*(x**3) + 36*(x**2) - 24*x + 5
+H3_2 = 4*(x**3) - 10*(x**2) + 8*x - 2
+H4_2 = 4*(x**3) - 8*(x**2) + 5*x - 1
+
+K = np.empty([4, 4])
+for e in range(num_elem):
+		for i in range(4):
+				for j in range(4):
+						integrand = sp.diff(eval("H{0}_{1}".format(i+1, e+1)), x, 2) * sp.diff(eval("H{0}_{1}".format(j+1, e+1)), x, 2)
+						K[i, j] = sp.integrate(integrand, (x, p[e, 0], p[e, 1]))
+
