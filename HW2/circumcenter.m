d9b5a9d17f0ce3a33c7d8dd56d6df4d636761437
aprilnovak <novak@berkeley.edu>
2017-02-10 10:09:51 -0800

    added plotting functions from previous assignment.

---
+++ HW2/circumcenter.m
@@ -0,0 +1,34 @@
+function [pt] = circumcenter(A, B, C, pv)
+
+AB = [(A(1) + B(1)) / 2, (A(2) + B(2)) / 2];
+m_AB = (B(2) - A(2)) / (B(1) - A(1));
+
+AC = [(A(1) + C(1)) / 2, (A(2) + C(2)) / 2];
+m_AC = (C(2) - A(2)) / (C(1) - A(1));
+
+BC = [(B(1) + C(1)) / 2, (B(2) + C(2)) / 2];
+m_BC = (C(2) - B(2)) / (C(1) - B(1));
+
+if m_BC == 0
+    m_AB = -1/m_AB;
+    m_AC = -1/m_AC;
+    mat = [1, -m_AC; 1, -m_AB];
+    b = [-m_AC * AC(1) + AC(2); -m_AB * AB(1) + AB(2)];
+elseif m_AC == 0
+    m_AB = -1/m_AB;
+    m_BC = -1/m_BC;
+    mat = [1, -m_BC; 1, -m_AB];
+    b = [-m_BC * BC(1) + BC(2); -m_AB * AB(1) + AB(2)];
+else
+    m_AC = -1/m_AC;
+    m_BC = -1/m_BC;
+    mat = [1, -m_BC; 1, -m_AC];
+    b = [-m_BC * BC(1) + BC(2); -m_AC * AC(1) + AC(2)];
+end
+
+circumcenter = transpose(mat\b);
+pt(1) = circumcenter(2);
+pt(2) = circumcenter(1);
+
+end
+
---
+++ HW2/pmesh.m
@@ -0,0 +1,86 @@
+function [p, t, e] = pmesh(pv, hmax, nref)
+% Uses Delaunay triangulation and refinement to mesh a polygon with
+% vertices pv, maximum element side length hmax, and number of uniform
+% refinements nref. The return values of this function are the node points
+% p, the triangle indices t, and the indices of the boundary points e.
+
+% place points on the domain boundaries according to hmax
+[new_pts] = initial_mesh(pv, hmax);
+
+% assemble all of the starting node points
+pv_orig = pv(1:end-1, :);
+pv = [pv_orig; new_pts; 2,2];
+
+max = hmax^2 / 2;
+max_area = max + 1; % dummy initial value
+while max_area > max
+    % find which points are outside the domain, then delete them
+    pv = unique(pv, 'rows');
+    [pv] = delete_outside(pv, pv_orig);
+    
+    % triangulate the domain
+    T = delaunayn(pv);
+
+    % find which triangles are outside the domain, then delete them from T
+    [T] = delete_outside_triangles(T, pv, pv_orig);
+    %tplot(pv, T)
+    
+    % compute triangle areas using Heron's formula
+    for i = 1:length(T(:,1))
+        A = [pv(T(i, 1), 1), pv(T(i, 1), 2)];
+        B = [pv(T(i, 2), 1), pv(T(i, 2), 2)];
+        C = [pv(T(i, 3), 1), pv(T(i, 3), 2)];
+        area(i) = (A(1) * (B(2) - C(2)) + B(1) * (C(2) - A(2)) + C(1) * (A(2) - B(2))) / 2;
+        if i == 1
+            max_area = area(1);
+        end
+        
+        if area(i) > max_area
+            max_area = area(i);
+            refine = i;
+        end 
+    end
+
+    A = [pv(T(refine, 1), 1), pv(T(refine, 1), 2)];
+    B = [pv(T(refine, 2), 1), pv(T(refine, 2), 2)];
+    C = [pv(T(refine, 3), 1), pv(T(refine, 3), 2)];
+    
+    [pt] = circumcenter(A, B, C, pv);
+    pv = [pv; pt];
+end
+
+% remove last triangulation (not needed since we just broke from loop)
+pv = pv(1:(end-1), :);
+%tplot(pv, T)
+
+% perform uniform refinements
+for uf = 1:nref
+    sprintf('Performing uniform refinement %i', uf)
+
+    for i = 1:length(T(:,1))
+        A = [pv(T(i, 1), 1), pv(T(i, 1), 2)];
+        B = [pv(T(i, 2), 1), pv(T(i, 2), 2)];
+        C = [pv(T(i, 3), 1), pv(T(i, 3), 2)];
+        
+        % add the three midpoints
+        pv = [pv; (A(1) + B(1))/2, (A(2) + B(2))/2];
+        pv = [pv; (A(1) + C(1))/2, (A(2) + C(2))/2];
+        pv = [pv; (C(1) + B(1))/2, (C(2) + B(2))/2];
+    end
+    
+    pv = unique(pv, 'rows');
+    T = delaunayn(pv);
+    
+    % find which triangles are outside the domain, then delete them from T
+    [T] = delete_outside_triangles(T, pv, pv_orig);
+    
+    %tplot(pv, T)
+    %hold on
+end
+
+p = pv;
+t = T;
+e = boundary_nodes(T);
+
+end
+
