8ff91c116042a4042e2a356173a7568551c1bd53
aprilnovak <novak@berkeley.edu>
2017-02-20 13:12:24 -0800

    added function to find row number given point.

--- HW3/p2mesh.m
+++ HW3/p2mesh.m
@@ -14,11 +14,10 @@ e = [1, 2, 3, 4, 5];
 tplot(p, t)
 hold on
 
-tol = 1e-10;
-
-% initialize new triangulation
+% initialize new triangulation and the new nodes and boundary nodes
 T = zeros(length(t(:,1)), 6);
 pnew = [];
+enew = [];
 
 % add all of the midpoints
 for i = 1:length(t(:,1))
@@ -34,6 +33,7 @@ for i = 1:length(t(:,1))
     T(i, 1) = t(i, 1);
     T(i, 3) = t(i, 2);
     T(i, 5) = t(i, 3);
+    
 end
 
 % p2 contains both the original mesh points and all of the midpoints
@@ -58,21 +58,58 @@ for i = 1:length(t(:,1))
     pt2 = [(C(1) + B(1))/2, (C(2) + B(2))/2];
     pt3 = [(A(1) + C(1))/2, (A(2) + C(2))/2];
     
-    % find the row number of each point in p
-    for j = 1:length(p2(:,1))
-        % check pt1
-        if (abs(pt1(1) - p2(j, 1)) < tol) && (abs(pt1(2) - p2(j, 2)) < tol)
-            T(i, 2) = j;
-        end
-        
-        % check pt2
-        if (abs(pt2(1) - p2(j, 1)) < tol) && (abs(pt2(2) - p2(j, 2)) < tol)
-            T(i, 4) = j;
-        end
-        
-        % check pt3
-        if (abs(pt3(1) - p2(j, 1)) < tol) && (abs(pt3(2) - p2(j, 2)) < tol)
-            T(i, 6) = j;
-        end
-    end
+    T(i, 2) = row_number(pt1, p2);
+    T(i, 4) = row_number(pt2, p2);
+    T(i, 6) = row_number(pt3, p2);
+    
+%     % find the row number of each point in p
+%     for j = 1:length(p2(:,1))
+%         % check pt1
+%         if (abs(pt1(1) - p2(j, 1)) < tol) && (abs(pt1(2) - p2(j, 2)) < tol)
+%             T(i, 2) = j;
+%         end
+%         
+%         % check pt2
+%         if (abs(pt2(1) - p2(j, 1)) < tol) && (abs(pt2(2) - p2(j, 2)) < tol)
+%             T(i, 4) = j;
+%         end
+%         
+%         % check pt3
+%         if (abs(pt3(1) - p2(j, 1)) < tol) && (abs(pt3(2) - p2(j, 2)) < tol)
+%             T(i, 6) = j;
+%         end
+%     end
+    
+    
+    % the new boundary nodes are the midpoints between nodes that were
+    % previously on the boundary
+    
+    % check if side 1-2 or 1-3 is on the boundary (both nodes in e)
+%     for j = 1:length(e)
+%         if e(j) == t(i, 1) % the 1-coordinate is on the boundary
+%             for k = 1:length(e)
+%                 % either the 2 or 3-coordinate is on boundary
+%                 if e(k) == t(i, 2)
+%                     enew = [enew; pt1];
+%                 end
+%                 if e(k) == t(i, 3)
+%                     enew = [enew; pt3];
+%                 end
+%             end
+%         end 
+%     end
+    
+%     % check if side 2-3 is on the boundary (both nodes in e)
+%     for j = 1:length(e)
+%         if e(j) == t(i, 2) % the 2-coordinate is on the boundary
+%             for k = 1:length(g)
+%                 if e(k) == t(i, 3) % the 3-coordinate is on the boundary
+%                     enew = [enew; pt2];
+%                 end
+%             end
+%         end
+%     end
 end
+
+
+
