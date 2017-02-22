bb81f61dd57dcd2bac3ef8509d38e1043bb42e1e
aprilnovak <novak@berkeley.edu>
2017-02-21 20:39:33 -0800

    copied over FEM files from last hw.

---
+++ HW3/fempoi2.m
@@ -0,0 +1,88 @@
+%function [a] = fempoi2(p, t, e)
+%  p = coordinates in mesh
+%  t = triangulation
+%  e = Dirichlet boundary nodes
+%  --- To use this function, you must have already run pmesh()
+pv = [0,0; 1,0; 1,1; 0,1; 0,0];
+[p, t, e] = pmesh(pv, 0.15, 0);
+
+LM = t;
+coordinates = p;
+num_elem = length(LM(:,1)); 
+
+dirichlet_nodes(1,:) = e;       % specify Dirichlet nodes
+num_nodes_per_elem = 3;         % quadratic triangular elements
+
+% form the permutation matrix for assembling the global matrices
+[perm] = permutation(num_nodes_per_elem);
+
+% specify the boundary conditions - homogeneous neumann and dirichlet
+dirichlet_nodes(2,:) = zeros .* dirichlet_nodes(1,:);
+a_k = dirichlet_nodes(2,:);
+num_nodes = length(p(:,1));
+
+% specify the quadrature rule
+wt = [1/2];
+qp = [1/3];
+
+% assemble the elemental k and elemental f
+K = zeros(num_nodes);
+F = zeros(num_nodes, 1);
+
+for elem = 1:num_elem
+    k = 0;
+    f = 0;
+
+    for ll = 1:length(qp) % eta loop
+         for l = 1:length(qp) % xe loop
+                 [N, dN_dxe, dN_deta, x_xe_eta, y_xe_eta, dx_dxe, dx_deta, dy_dxe, dy_deta, B] = shapefunctions(qp(l), qp(ll), num_nodes_per_elem, coordinates, LM, elem);
+                 F_mat = transpose([dx_dxe, dx_deta; dy_dxe, dy_deta]);
+                 J = det(F_mat);
+
+                 % assemble the (elemental) forcing vector
+                 f = f + wt(ll) * wt(l) * transpose(N) * J;
+
+                 % assemble the (elemental) stiffness matrix - correct
+                 k = k + wt(ll) * wt(l) * transpose(inv(F_mat) * B) * inv(F_mat) * B * J;      
+         end
+    end
+    
+    % place the elemental k matrix into the global K matrix
+    for m = 1:length(perm(:,1))
+       i = perm(m,1);
+       j = perm(m,2);
+       K(LM(elem, i), LM(elem, j)) = K(LM(elem, i), LM(elem, j)) + k(i,j);
+    end
+
+    % place the elemental f matrix into the global F matrix
+    for i = 1:length(f)
+       F(LM(elem, i)) = F((LM(elem, i))) + f(i);
+    end
+end
+
+% perform static condensation to remove known Dirichlet nodes from solve
+[K_uu, K_uk, F_u, F_k] = condensation(K, F, num_nodes, dirichlet_nodes);
+
+% perform the solve
+a_u_condensed = K_uu \ (F_u - K_uk * dirichlet_nodes(2,:)');
+
+% expand a_condensed to include the Dirichlet nodes
+a = zeros(num_nodes, 1);
+
+a_row = 1;
+i = 1;      % index for dirichlet_nodes
+j = 1;      % index for expanded row
+
+for a_row = 1:num_nodes
+    if (find(dirichlet_nodes(1, :) == a_row))
+        a(a_row) = dirichlet_nodes(2,i);
+        i = i + 1;
+    else
+        a(a_row) = a_u_condensed(j);
+        j = j + 1;
+    end
+end
+
+tplot(p, LM, a)
+
+%end
---
+++ HW3/shapefunctions.m
@@ -0,0 +1,53 @@
+function [N, dN_dxe, dN_deta, x_xe_eta, y_xe_eta, dx_dxe, dx_deta, dy_dxe, dy_deta, B] = shapefunctions(xe, eta, num_nodes_per_elem, coordinates, LM, elem)
+
+N = zeros(num_nodes_per_elem, 1);
+dN = zeros(num_nodes_per_elem, 1);
+
+switch num_nodes_per_elem
+    case 3
+        N(1) = 1 - xe - eta;
+        N(2) = xe;
+        N(3) = eta;
+        dN_dxe(1) = -1;
+        dN_dxe(2) = 1;
+        dN_dxe(3) = 0;
+        dN_deta(1) = -1;
+        dN_deta(2) = 0;
+        dN_deta(3) = 1;
+        B = [dN_dxe(1), dN_dxe(2), dN_dxe(3); dN_deta(1), dN_deta(2), dN_deta(3)];
+    case 4
+        N(1) = 0.25 * (1 - xe) * (1 - eta);
+        N(2) = 0.25 * (1 + xe) * (1 - eta);
+        N(3) = 0.25 * (1 + xe) * (1 + eta);
+        N(4) = 0.25 * (1 - xe) * (1 + eta);
+        dN_dxe(1) = 0.25 * -(1 - eta);
+        dN_dxe(2) = 0.25 * (1 - eta);
+        dN_dxe(3) = 0.25 * (1 + eta);
+        dN_dxe(4) = 0.25 * -(1 + eta);
+        dN_deta(1) = 0.25 * (1 - xe) * -1;
+        dN_deta(2) = 0.25 * (1 + xe) * -1;
+        dN_deta(3) = 0.25 * (1 + xe) * 1;
+        dN_deta(4) = 0.25 * (1 - xe) * 1;
+        B = [dN_dxe(1), dN_dxe(2), dN_dxe(3), dN_dxe(4); dN_deta(1), dN_deta(2), dN_deta(3), dN_deta(4)];
+    otherwise
+        disp('You entered an unsupported number of nodes per element.');
+end
+
+% x(xe, eta) and y(xe, eta) transformation to the parametric domain
+x_xe_eta = 0.0;
+y_xe_eta = 0.0;
+dx_dxe = 0.0;
+dy_dxe = 0.0;
+dx_deta = 0.0;
+dy_deta = 0.0;
+
+for i = 1:num_nodes_per_elem
+    x_xe_eta = x_xe_eta + coordinates(LM(elem, i), 1) * N(i);
+    y_xe_eta = y_xe_eta + coordinates(LM(elem, i), 2) * N(i);
+    
+    dx_dxe   = dx_dxe   + coordinates(LM(elem, i), 1) * dN_dxe(i);
+    dy_dxe   = dy_dxe   + coordinates(LM(elem, i), 2) * dN_dxe(i);
+    
+    dx_deta = dx_deta   + coordinates(LM(elem, i), 1) * dN_deta(i);
+    dy_deta = dy_deta   + coordinates(LM(elem, i), 2) * dN_deta(i);
+end
