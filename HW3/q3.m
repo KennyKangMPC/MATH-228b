c666e685be8977e3f6783a94dc0193de81e2544e
aprilnovak <novak@berkeley.edu>
2017-02-26 19:12:42 -0800

    finished Question 3, part b.

--- HW3/q3.m
+++ HW3/q3.m
@@ -6,12 +6,26 @@ pv = [0,0; 5,0; 5,1; 3.1,1; 3.1,0.2; 2.9,0.2; 2.9,1; 2.1,1; 2.1,0.2; 1.9,0.2; 1.
 % find the mesh edges
 [ein, eout, ewall] = waveguide_edges(p, t);
 
+H = [];
+Wave = 6:0.01:6.5;
+
 % solve femhelmholtz
-wave = 6;
+for wave = Wave
 
-[K, M, Bin, Bout, bin] = femhelmholtz(p, t, ein, eout, wave);
-Kk = K - (wave.^2) .* M + 1i .* wave .* (Bin + Bout);
-Ff = bin .* 2 .* 1i .* wave;
-a = Kk\Ff;
+    [K, M, Bin, Bout, bin] = femhelmholtz(p, t, ein, eout, wave);
+    Kk = K - (wave.^2) .* M + 1i .* wave .* (Bin + Bout);
+    Ff = bin .* 2 .* 1i .* wave;
+    a = Kk\Ff;
 
-tplot(p, t, real(a))
+    % compute complex conjugate transpose
+    a_h = transpose(conj(a));
+
+    % compute H(u)
+    H = [H, a_h * Bout * a];
+end
+
+semilogy(Wave, real(H), 'o-')
+xlabel('k')
+ylabel('log(H(u))')
+grid on
+%tplot(p, t, real(a))
