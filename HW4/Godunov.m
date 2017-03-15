98aecfaa9098d9f2828e3d4c3cd5ba4d89fed3ad
aprilnovak <novak@berkeley.edu>
2017-03-15 15:53:04 -0700

    fixed error in interpretation of Godunov method

--- HW4/Godunov.m
+++ HW4/Godunov.m
@@ -1,15 +1,37 @@
-function [F_left, F_right] = Godunov(flux_left, flux_mid, flux_right, left_point, mid_point, right_point)
+function [F_left, F_right] = Godunov(left_point, mid_point, right_point, p_max)
+maxpt = p_max/2;
+maxflux = flux(maxpt);
+
+flux_left = flux(left_point);
+flux_mid = flux(mid_point);
+flux_right = flux(right_point);
 
 if left_point < mid_point
-    F_left = min(flux_left, flux_mid);
+    if ((maxpt > left_point) && (maxpt < mid_point))
+        F_left = min([flux_left, flux_mid, maxflux]);
+    else
+        F_left = min([flux_left, flux_mid]);
+    end
 else
-    F_left = max(flux_left, flux_mid);
+    if ((maxpt < left_point) && (maxpt > mid_point))
+        F_left = max([flux_left, flux_mid, maxflux]);
+    else
+        F_left = max([flux_left, flux_mid]);
+    end
 end
 
 if mid_point < right_point
-    F_right = min(flux_mid, flux_right);
+    if ((maxpt > mid_point) && (maxpt < right_point))
+        F_right = min([flux_left, flux_mid, maxflux]);
+    else
+        F_right = min([flux_mid, flux_right]);
+    end
 else
-    F_right = max(flux_mid, flux_right);
+    if ((maxpt < mid_point) && (maxpt > right_point))
+        F_right = max([flux_left, flux_mid, maxflux]);
+    else
+        F_right = max([flux_mid, flux_right]);
+    end
 end
 
 end
--- HW4/Q1.m
+++ HW4/Q1.m
@@ -16,8 +16,7 @@ time = 0:dt:T;
 backsize = length(mesh(mesh < 0.0));
 frontsize = length(mesh) - backsize;
 
-pG = cell(length(time), 1);
-pR = cell(length(time), 1);
+pG = cell(length(time), 1); pR = cell(length(time), 1);
 for i = 1:length(time)
     pG{i} = [p_left .* ones(1, backsize), p_right .* ones(1, frontsize)];
     pR{i} = [p_left .* ones(1, backsize), p_right .* ones(1, frontsize)];
@@ -36,16 +35,8 @@ for t = 1:length(time)
             left_pointR = pR{t}(i - 1);
         end
         
-        flux_leftG = flux(left_pointG);
-        flux_midG = flux(pG{t}(i));
-        flux_rightG = flux(pG{t}(i + 1));
-        
-        flux_leftR = flux(left_pointR);
-        flux_midR = flux(pR{t}(i));
-        flux_rightR = flux(pR{t}(i + 1));
-        
-        [F_leftG(i), F_rightG(i)] = Godunov(flux_leftG, flux_midG, flux_rightG, left_pointG, pG{t}(i), pG{t}(i+1));
-        [F_leftR(i), F_rightR(i)] = Roe(flux_leftR, flux_midR, flux_rightR, left_pointR, pR{t}(i), pR{t}(i+1));
+        [F_leftG(i), F_rightG(i)] = Godunov(left_pointG, pG{t}(i), pG{t}(i+1), p_max);
+        [F_leftR(i), F_rightR(i)] = Roe(left_pointR, pR{t}(i), pR{t}(i+1));
         
     pG{t + 1}(i) = pG{t}(i) - (dt / dx) * (F_rightG(i) - F_leftG(i));
     pR{t + 1}(i) = pR{t}(i) - (dt / dx) * (F_rightR(i) - F_leftR(i));
