8c8cd4424e17b803c9607894f3f6a8f91f9f91ef
aprilnovak <novak@berkeley.edu>
2017-04-03 11:56:21 -0700

    added lines showing element boundaries.

--- HW5/dgconvect0.m
+++ HW5/dgconvect0.m
@@ -49,49 +49,50 @@ for el = 1:n
     x_plot(:, el) = linspace(x(1, el), x(end, el), 3*p);
 end
 
-u_plot = [];
-coeff_plot = [];
+[u_plot] = u_plot(n, p, u, x_plot, x);
+it = 1;
+uexact = uinit(mod(xx - dt*it, 1.0));
+plot(x, u, 'r', x_plot, u_plot, 'b', xx, uexact, 'k')
 
-% find expansion coefficients for each element
-for el = 1:n
-    A = [];
-    for i = 1:(p + 1)
-        A(i, 1:(p + 1)) = x(i, el) .^ ((1:(p + 1)) - 1);
+horiz = [[0, x(end, :)]; [0, x(end, :)]];
+vert = [0; 1] * ones(1, length([0, x(end, :)]));
+
+for it = 1:T/dt                                % Main time-stepping loop
+    % Runge-Kutta 4
+    k1 = dt * rhs(u, Kel, Mel);
+    k2 = dt * rhs(u + k1/2, Kel, Mel);
+    k3 = dt * rhs(u + k2/2, Kel, Mel);
+    k4 = dt * rhs(u + k3, Kel, Mel);
+    u = u + (k1 + 2*k2 + 2*k3 + k4) / 6;
+
+    
+
+    % Plotting
+    if mod(it, T/dt/1000) == 0                 % 1000 frames
+
+        uexact = uinit(mod(xx - dt*it, 1.0));  % Exact solution
+        u_plot = []; A = []; coeff_plot = [];
+
+        for el = 1:n
+            for i = 1:(p + 1)
+                A(i, 1:(p + 1)) = x(i, el) .^ ((1:(p + 1)) - 1);
+            end
+
+            coeff_plot(:, el) = A \ u(:, el);
+
+            for i = 1:length(x_plot(:, 1))
+                u_plot(i, el) = sum(coeff_plot(:, el)' .* (x_plot(i, el) .^ ((1:(p + 1)) - 1)));
+            end
+        end
+        
+        plot(x_plot, u_plot, 'b', xx, uexact, 'k', horiz, vert, '--')
+        grid on
+        axis([0, 1, -0.1, 1.1])
+        drawnow
     end
-
-    coeff_plot(:, el) = A \ u(:, el);
 end
 
-for el = 1:n
-    for i = 1:length(x_plot(:, 1))
-        u_plot(i, el) = sum(coeff_plot(:, el)' .* (x_plot(i, el) .^ ((1:(p + 1)) - 1)));
-    end
-end
+uexact = uinit(mod(x - T, 1.0));               % Exact final solution
+error = max(abs(u(:) - uexact(:)));            % Discrete inf-norm error
 
-%plot(x, u, 'r', x_plot, u_plot, 'b')
-
-% for it = 1:T/dt                                % Main time-stepping loop
-%     % Runge-Kutta 4
-%     k1 = dt * rhs(u, Kel, Mel);
-%     k2 = dt * rhs(u + k1/2, Kel, Mel);
-%     k3 = dt * rhs(u + k2/2, Kel, Mel);
-%     k4 = dt * rhs(u + k3, Kel, Mel);
-%     u = u + (k1 + 2*k2 + 2*k3 + k4) / 6;
-% 
-%     
-% 
-%     % Plotting
-%     if mod(it, T/dt/1000) == 0                 % 1000 frames
-
-%         uexact = uinit(mod(xx - dt*it, 1.0));  % Exact solution
-%         plot(x, u, 'b', xx, uexact, 'k')
-%         grid on
-%         %axis([0, 1, -0.1, 1.1])
-%         drawnow
-%     end
-% end
-% 
-% uexact = uinit(mod(x - T, 1.0));               % Exact final solution
-% error = max(abs(u(:) - uexact(:)));            % Discrete inf-norm error
-% 
-% % compute the error as the (integrated) L-2 norm   
+% compute the error as the (integrated) L-2 norm   
