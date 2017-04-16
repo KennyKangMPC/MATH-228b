cc8d366237dd21a051f6bdd1a42bb25401ac0f68
aprilnovak <novak@berkeley.edu>
2017-04-16 15:42:18 -0700

    finished question 2, part a.

--- HW6/gauss_seidel.m
+++ HW6/gauss_seidel.m
@@ -1,3 +1,15 @@
-% HW 6
-[p,t,e] = pmesh([0,0; 1,0; 1,1; 0,1; 0,0], 0.15, 0);
-[a] = fempoi(p, t, e);
+function [u, res] = gauss_seidel(A, b, u, niter)
+% A - matrix in A * u = b
+% b - vector in A * u = b
+% u - initial Gauss-Seidel solution guess
+% niter - number of Gauss-Seidel iterations
+
+% Gauss-Seidel solution to Au = b
+res = zeros(1, niter + 1);
+res(1) = max(b - A * u);
+
+for it = 1:niter
+    u = u + inv(tril(A)) * (b - A * u);
+    res(it + 1) = max(b - A * u);
+end
+
