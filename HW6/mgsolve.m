b03e14ef0019ba62b4790d422d544b30d38a1608
aprilnovak <novak@berkeley.edu>
2017-04-16 20:18:04 -0700

    wrote basic algorithm.

--- HW6/mgsolve.m
+++ HW6/mgsolve.m
@@ -2,8 +2,31 @@ clear all
 
 % specify the mesh
 pv   = [0,0; 1,0; 1,1; 0,1; 0,0];
-hmax = 0.1;
+hmax = 0.75;
 nref = 1;
 
 % precompute all necessary matrices
 data = mginit(pv, hmax, nref);
+% data(i).T promotes from i to i + 1
+% data(i).R reduces from i + 1 to i
+
+% perform first G-S iterations (10 relaxations)
+[u, res] = gauss_seidel(data(nref + 1).A, data(nref + 1).b, 0 .* data(nref + 1).b, 10);
+
+% compute a residual
+r = data(nref + 1).b - data(nref + 1).A * u;
+
+% restrict
+r = data(nref).R * r;
+
+% with this coarser residual, solve the error equation on the coarser mesh
+e = data(nref).A \ r;
+
+% transform this error back to the fine mesh
+e = data(nref).T * e;
+
+% correct the iterative solution using this error
+u = u + e;
+
+% now, relax again using the updated guess
+
