1ebd8b05c9b18461a75a3ce89c52cd812a444442
aprilnovak <novak@berkeley.edu>
2017-04-16 21:44:16 -0700

    made while() loop that will continue until reaching the zero-th refinement level.

--- HW6/mgsolve.m
+++ HW6/mgsolve.m
@@ -5,7 +5,7 @@ pv   = [0,0; 1,0; 1,1; 0,1; 0,0];
 hmax = 0.75;
 nref = 4;
 
-niter = 1000;
+niter = 100;
 % precompute all necessary matrices
 data = mginit(pv, hmax, nref);
 % data(i).T promotes from i to i + 1
@@ -25,7 +25,42 @@ r = data(i).R * r;
 % continually solve G-S and then coarsen using recursion. We want to output
 % the error vector of the coarsest mesh using recursion.
 i = i - 1;
-[e] = restrict(data, r, niter, i);
+%[e] = restrict(data, r, niter, i);
+
+% function that performs some G-S iterations to solve A * u = rhs
+% and then computes a residual r = rhs - A * u, and then restricts it
+
+while i > 0
+    [e] = gauss_seidel(data(i + 1).A, r, 0 .* r, niter);
+
+    % compute a residual, then restrict
+    r = r - data(i + 1).A * e;
+    r = data(i).R * r;
+    i = i - 1;
+end
+
+ge = data(i + 1).A \ r;
+
+% if i == 1
+%     e = data(i).A \ r;
+%     return
+% else
+%     [e] = gauss_seidel(data(i + 1).A, r, 0 .* r, niter);
+% 
+%     % compute a residual, then restrict
+%     r = r - data(i + 1).A * e;
+%     r = data(i).R * r;
+%     i = i - 1;
+% end
+
+
+
+
+
+
+
+
+
 
 
 % solve the error equation with this residual
