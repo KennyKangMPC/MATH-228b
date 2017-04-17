a39c25c37d8249ec8df82313c8acb251af7c4d19
aprilnovak <novak@berkeley.edu>
2017-04-16 19:51:19 -0700

    finished parts c, question 2.

--- HW6/mginit.m
+++ HW6/mginit.m
@@ -1,26 +1,17 @@
 % mginit
 clear all
 
+nref = 1;
 % specify the mesh
 pv = [0,0; 1,0; 1,1; 0,1; 0,0];
-[p, t, e, data, pmid, ia] = pmesh(pv, 0.75, 1);
+[p, t, e, data] = pmesh(pv, 0.75, 1);
 
-
-
-% % test out the T matrix acting on a linear function z = x + y
-% x = data(ref).p(:, 1);
-% y = data(ref).p(:, 2);
-% funct = x.*x + y.*y; 
-% 
-% %tplot(data(ref).p, data(ref).t, funct)
-% 
-% funct_interp = T * funct;
-%tplot(data(ref + 1).p, data(ref + 1).t, funct_interp)
-
-%plot(1:length(funct), funct, 'o-', 1:length(funct_interp), funct_interp, '*-')
-
+% determine the A and b matrices by calling fempoi
+for i = 1:(nref + 1)
+    [data(i).u, data(i).A, data(i).b] = fempoi(data(i).p, data(i).t, data(i).e);
+end
 % perform the solve to get the true solution u0
-%[u_true, A, b] = fempoi(p, t, e);
+
 %tplot(p, t, u_true)
 
 %[u, res] = gauss_seidel(A, b, 0 .* u_true, 100);
