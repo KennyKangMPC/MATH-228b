abcbc741372e89d8c9904c32d4e2d7e3a73a29a5
aprilnovak <novak@berkeley.edu>
2017-04-17 11:43:42 -0700

    minor changes, still doesn't work.

--- HW6/mgsolve.m
+++ HW6/mgsolve.m
@@ -21,7 +21,8 @@ soln = data(nref + 1).A \ data(nref + 1).b;
 res            = [];
 loop_iter      = 1;
 data(nref + 1).u = 0 .* data(nref + 1).b;
-res(loop_iter) = max(abs(data(nref + 1).b - data(nref + 1).A * data(nref + 1).u));
+data(nref + 1).r = data(nref + 1).b - data(nref + 1).A * data(nref + 1).u;
+res(loop_iter) = max(abs(data(nref + 1).r));
 
 while res(loop_iter) > tol
     i = nref;
@@ -34,15 +35,10 @@ while res(loop_iter) > tol
     
     % restrict to a coarser mesh
     data(i).r = data(i).R * data(i + 1).r;
-    
-    % compute an initial e_h (because it won't be computed in the next loop)
-    %data(nref + 1).err = 0.* data(nref + 1).u;
 
-    % continually solve G-S and then coarsen using recursion. We want to output
-    % the error vector of the coarsest mesh using recursion.
     i = i - 1;
     while i > 0
-        % smoothing (initial guess is zero)
+        % smoothing the error equation (initial guess is zero)
         [data(i + 1).err] = gauss_seidel(data(i + 1).A, data(i + 1).r, 0 .* data(i + 1).r, vdown);
 
         % compute a residual
@@ -57,8 +53,7 @@ while res(loop_iter) > tol
     % analytically solve on the coarsest mesh
     data(1).err = data(1).A \ data(1).r;
 
-    % now that we are at the coarsest mesh, we need to interpolate upwards the 
-    % error so that we can add it to the solution iterate
+    % interpolate upwards
     for i = 1:(nref - 1)
         % project back up
         data(i + 1).err = data(i + 1).err + data(i).T * data(i).err;
@@ -69,26 +64,19 @@ while res(loop_iter) > tol
     
     % at the last point, the equation is u_h = u_h + e_h;
     data(nref + 1).u = data(nref + 1).u + data(nref).T * data(nref).err;
-  
-    % perform G-S iterations to improve projected-up solution
-    %[data(i + 1).err] = gauss_seidel(data(i + 1).A, data(i + 1).r, data(i + 1).err, vup);
     
-
-    %data(nref + 1).u = data(nref + 1).u + data(nref + 1).err;
-    
-    % perform G-S post-processing to obtain closer solution
-    %[data(i + 1).u] = gauss_seidel(data(i + 1).A, data(i + 1).b, data(i + 1).u, vup);
+    % smooth u_h
+    [data(nref + 1).u] = gauss_seidel(data(nref + 1).A, data(nref + 1).b, data(nref + 1).u, vup);
     
     loop_iter = loop_iter + 1;
     
     % compute the residual for this newest iterate
     res(loop_iter) = max(abs(data(nref + 1).b - data(nref + 1).A * data(nref + 1).u));
-    %res
 end
 
 sol = data(nref + 1).u;
 % plot difference from backslash solution
-%tplot(data(nref + 1).p, data(nref + 1).t, abs(u - soln))
+%tplot(data(nref + 1).p, data(nref + 1).t, abs(sol - soln))
 
 
 end
--- HW6/myscr.m
+++ HW6/myscr.m
@@ -1,7 +1,8 @@
 clear all
 
-pv = [0,0; 2,0; 1.5,1; .5,1; 0,0];
-for iref = 1:5
+%pv = [0,0; 2,0; 1.5,1; .5,1; 0,0];
+pv = [0,0; 1,0; 1,1; 0,1; 0,0];
+for iref = 1:3
     sprintf('Beginning refinement level %i', iref)
     data = mginit(pv, 0.5, iref);
     [u, res] = mgsolve(data, 2, 2, 1e-10, iref);
