33098641d0078c8deaa679ac3d6592339ad0f288
aprilnovak <novak@berkeley.edu>
2017-04-18 20:28:16 -0700

    got part 1 to work

--- HW6/eikonal.m
+++ HW6/eikonal.m
@@ -1,24 +1,52 @@
 % solve the Eikonal equation on unit square
+clear all
 X = 1;
 Y = 1;
 
-dx = 0.1;           % mesh spacing
-dy = 0.1;
-dt = 0.1;           % time step
+dx = 0.01;           % mesh spacing
+dy = 0.01;
+dt = 1e-3;           % time step
 x = 0:dx:X;         % Cartesian grid
 y = 0:dy:Y;         % Cartesian grid
+[XX, YY] = meshgrid(x, y);
 numx = length(x);   
 numy = length(y);   
 
 F   = 1.0 .* ones(numx, numy);
-phi = zeros(numx, numy);
+phi = 0.0 .* ones(numx, numy);
+initx = 0.2;
+inity = 0.2;
+norm = 1.0; % arbitrary initial value
+tol = 1e-6; % iteration tolerance
+ifig = 0;
 
-for i = 1:length(x) % loop over x-coordinates
-    for j = 1:length(y) % loop over y-coordinates
-        plus = grad_plus(phi, i, j, dx, dy, numx, numy);
-        phi(i, j) = phi(i, j) - dt * (max(F(i, j), 0) * plus + min(F(i, j), 0)) + dt;
+while norm > tol
+    phi_prev = phi;
+    
+    for i = 1:length(x) % loop over x-coordinates
+        for j = 1:length(y) % loop over y-coordinates
+            plus = grad_plus(phi, i, j, dx, dy, numx, numy);
+            phi(i, j) = phi(i, j) - dt * (max(F(i, j), 0) * plus) + dt;
+        end
     end
+    
+    ifig = ifig + 1;
+    if ifig == 10 % plot every 10 frames
+        ifig = 0;
+        % plot the contours
+        %contour(XX, YY, phi)
+        % plot the surface
+        hSurf = surf(XX,YY,phi,'EdgeColor','none','LineStyle','none','FaceLighting','phong');
+        drawnow
+    end
+    
+    % apply the only boundary condition
+    phi(initx/dx, inity/dy) = 0.0;
+    
+    % find the L2-norm of the solution
+    norm = sum(sum(abs(phi_prev - phi) .^ 2));
 end
+
 
 
 
--- HW6/grad_plus.m
+++ HW6/grad_plus.m
@@ -1,27 +1,28 @@
 function plus = grad_plus(phi, i, j, dx, dy, numx, numy)
+    big = 1000;
     if i == 1
-        a = max(phi(i, j)/dx, 0); % remove undefined variable
+        a = max(phi(i, j)/dx - big, 0) ^ 2;
     else
-        a = max((phi(i, j) - phi(i - 1, j))/dx, 0);
+        a = max((phi(i, j) - phi(i - 1, j))/dx, 0) ^ 2;
     end
     
     if i == numx
-        b = min(-phi(i, j)/dx, 0); % remove undefined
+        b = min(big - phi(i, j)/dx, 0) ^ 2; % remove undefined
     else
-        b = min((phi(i + 1, j) - phi(i, j))/dx, 0); 
+        b = min((phi(i + 1, j) - phi(i, j))/dx, 0) ^ 2; 
     end
     
     if j == 1
-        c = max((phi(i, j)) / dy, 0); % remove undefined
+        c = max((phi(i, j) - big) / dy, 0) ^ 2; % remove undefined
     else
-        c = max((phi(i, j) - phi(i, j - 1)) / dy, 0);
+        c = max((phi(i, j) - phi(i, j - 1)) / dy, 0) ^ 2;
     end
     
     if j == numy
-        d = min(-phi(i, j) / dy, 0);
+        d = min(big -phi(i, j) / dy, 0) ^ 2;
     else
-        d = min((phi(i, j + 1) - phi(i, j)) / dy, 0);
+        d = min((phi(i, j + 1) - phi(i, j)) / dy, 0) ^ 2;
     end
         
-    plus = sqrt(a.^2 + b.^2 + c.^2 + d.^2);
+    plus = sqrt(a + b + c + d);
 end
