0e05fc18128259db6807d2ff43bbe38c08949bda
aprilnovak <novak@berkeley.edu>
2017-05-02 12:58:21 -0700

    finished euler_rhs.

--- HW7/euler_fluxes.m
+++ HW7/euler_fluxes.m
@@ -1,12 +1,12 @@
-function [Frx, Fry, Frux, Frvx, Frvy, FrEx, FrEy] = euler_fluxes(r, ru, rv, rE)
+function [Frx, Fry, Frux, Fruy, Frvx, Frvy, FrEx, FrEy] = euler_fluxes(r, ru, rv, rE)
 
-u = ru / r; v = rv / r; E = rE / r;
+u = ru ./ r; v = rv ./ r; E = rE ./ r;
 
 % compute pressure from ideal gas law
 y = 7/5;
-P = (y - 1) * r * (E - (u^2 + v^2) / 2);
+P = (y - 1) .* r .* (E - (u.^2 + v.^2) / 2);
 
 Frx = ru;               Fry = rv;
-Frux = r * u^2 + P;     Fruy = ru * v;
-Frvx = ru * v;          Frvy = r * v^2 + P;
-FrEx = u * (rE + P);    FrEy = v * (rE + P);
+Frux = r .* u.^2 + P;   Fruy = ru .* v;
+Frvx = ru .* v;         Frvy = r .* v.^2 + P;
+FrEx = u .* (rE + P);   FrEy = v .* (rE + P);
